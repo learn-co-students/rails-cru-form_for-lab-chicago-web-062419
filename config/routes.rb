89d9419ce1c52c0ff91c94265d6ffce4bd8a2362
Philip Roth <50246817+rothberry@users.noreply.github.com>
2019-07-23 09:48:55 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,30 @@
 Rails.application.routes.draw do
+  get 'genres/index'
+
+  get 'genres/show'
+
+  get 'genres/edit'
+
+  get 'genres/new'
+
+  get 'artists/index'
+
+  get 'artists/show'
+
+  get 'artists/edit'
+
+  get 'artists/new'
+
+  get 'artist/index'
+
+  get 'artist/show'
+
+  get 'artist/edit'
+
+  get 'artist/new'
+
+  resources :genres
+  resources :songs
+  resources :artists
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
